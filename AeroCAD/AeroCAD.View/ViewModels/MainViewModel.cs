70fc1a1a8124ad72d9771100cbeee30b9e722b59
Primusz Péter <primuszp@gmail.com>
2026-04-21 18:44:50 +0200

    Simplify viewport refresh flow

--- AeroCAD/AeroCAD.View/ViewModels/MainViewModel.cs
+++ AeroCAD/AeroCAD.View/ViewModels/MainViewModel.cs
@@ -667,9 +667,7 @@ namespace Primusz.AeroCAD.View.ViewModels
 
         private void RefreshViewportVisuals()
         {
-            viewport.GetRubberObject()?.InvalidateVisual();
             viewport.RefreshView();
-            viewport.InvalidateVisual();
         }
 
         private bool HasSelectedGrips()
